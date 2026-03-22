21fc42b7c7b5f742391333f5163616b7d10dbcca
Youssef Salah <yousefsalahali@std.mans.edu.eg>
2026-03-22 14:35:39 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,179 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    });
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+
+/*==================== SERVICES MODAL ====================*/
+/*==================== SERVICES MODAL ====================*/
+
+const modalViews = document.querySelectorAll(".services__modal"),
+      modalBtns = document.querySelectorAll(".services__button"),
+      modalCloses = document.querySelectorAll(".services__modal-close");
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add("active-modal")
+}
+
+modalBtns.forEach((btn,i)=>{
+    btn.addEventListener("click", ()=>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((close)=>{
+    close.addEventListener("click", ()=>{
+        modalViews.forEach((modalView)=>{
+            modalView.classList.remove("active-modal")
+        })
+    })
+})
+/*==================== PORTFOLIO SWIPER ====================*/
+let swiperPortfolio = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+/*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    const sectionId = current.getAttribute("id"); // ضفت هنا const عشان ما يبقاش variable طاير
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+  const scrollUpElement = document.getElementById("scroll-up");
+  if (this.scrollY >= 560) {
+      if(scrollUpElement) scrollUpElement.classList.add("show-scroll");
+  } else {
+      if(scrollUpElement) scrollUpElement.classList.remove("show-scroll");
+  }
+}
+window.addEventListener("scroll", scrollUp);
+
+/*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-sun" : "uil-moon";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+  themeButton.classList[selectedIcon === "uil-sun" ? "add" : "remove"](iconTheme);
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+/* ================= CERTIFICATIONS MODAL ================= */
+let certModal = document.getElementById("certModal");
+let certImg = document.getElementById("certImg");
+let images = document.querySelectorAll(".cert-img");
+let closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+    img.onclick = () => {
+        certModal.style.display = "block";
+        certImg.src = img.src;
+    }
+});
+
+closeBtn.onclick = () => {
+    certModal.style.display = "none";
+};
+
+certModal.onclick = (e) => {
+    if (e.target === certModal) {
+        certModal.style.display = "none";
+    }
+};
